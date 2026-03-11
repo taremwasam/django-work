de8155b01c9c6ff56cfa8edc5972823977097884
unknown <taremwasam101@gmail.com>
2026-03-11 16:31:53 +0200

    from fifth to eigth step

--- EduFinApp/core/models.py
+++ EduFinApp/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Testing(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    created_at=models.DateTimeField(auto_now_add=true)
+    created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
