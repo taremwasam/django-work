7fd6d1346310f77f56e39cf075c1504fa5b7d4f3
unknown <taremwasam101@gmail.com>
2026-03-11 14:21:32 +0200

    fourth step of testing models in coreapp

--- EduFinApp/accounts/models.py
+++ EduFinApp/accounts/models.py
@@ -3,15 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    role = models.CharField(max_length=20, default='student')
     pass
 # Create your models here.
-from django.db import models
-
-class Account(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-
-    role = models.CharField(max_length=20, default='student')
-
-    def __str__(self):
-        return self.name
